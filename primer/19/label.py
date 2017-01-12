9442359cf709a1985ee838f93d608e4fa226343a
Lantianor <lantianor@126.com>
2017-01-12 17:33:29 +0800

    Update label.py

--- primer/19/label.py
+++ primer/19/label.py
@@ -1,10 +1,8 @@
 import Tkinter
 
 top = Tkinter.Tk()
-
 label = Tkinter.Label(top,text="input")
 label.pack()
-
 button = Tkinter.Button(top,text="QIUT",command=top.quit,bg="red",fg="white")
 button.pack(fill=Tkinter.X,expand=1)
 
